990b5c999a981cd2a98b3d2631c993304ba0a2b5
Hope Muller <Home@GodsMerciesAreNewEveryMorning.local>
2021-04-21 22:49:14 -0400

    Pre-processing edits, take out underclassmen in last years, etc.

--- doe.preprocess.2.R
+++ doe.preprocess.2.R
@@ -5,9 +5,9 @@ library(foreach)
 library(lubridate)
 
 # setwd to doe folder
-setwd("~/RANYCS/sasdata/development/kmt")
+# Kenny: setwd("~/RANYCS/sasdata/development/kmt")
 
-# load in all data
+# load in data
 raw.nsc <- read_csv('nsc_all.csv')
 nsc <- raw.nsc
 
@@ -20,6 +20,7 @@ doe.full <- raw.student
 
 # clean the data
 doe.full <- doe.full %>%
+
   # rename columns
   dplyr::rename(id = RANYCSID,
                 pov = ANYPOV,
@@ -43,26 +44,51 @@ doe.full <- doe.full %>%
                 sus.days = SUSTOTDAYS,
                 ela = ELASSC,
                 mth = MTHSSC) %>%
+  
   # change grade to numeric
   mutate(grade = as.numeric(grade),
          ela = as.numeric(ela),
          mth = as.numeric(mth),
          # recode gender male as 0
-         gen = ifelse(gen == 2, 0, 1)) %>%
-  # filter out grade 8 and above
+         gen = ifelse(gen == 2, 0, 1),
+         # code percentage days absent per year and percent days suspended per year
+         per.abs = abs/180,
+         per.sus = sus.days/180) %>%
+  
+  # filter out above grade 8
   filter(grade >= 8)
+
+# filter out students who were not going to graduate by 2019
+doe.full <- doe.full %>%
+  filter(!(grade < 12 & year = 2019) |
+           (grade < 11 & year == 2018) |
+           (grade < 10 & year == 2017))
+          
+# parse birth year to new column
+doe.full <- doe.full %>%
+  mutate(birth.yr = year(mdy(dob)))
+
+# create column to show if they moved mid-year
+doe.full <- doe.full %>%
+  mutate(mvd.mid = case_when(sch.fall != sch.spr ~ 1,
+                         sch.fall == sch.spr ~ 0))
+
+# add total school count
+doe.full <- doe.full %>%
+  group_by(id)%>%
+  dplyr::summarise(num.schools = n_distinct(sch.fall))
 
 # status for each year and "any" flags
 doe.full <- doe.full %>% 
   group_by(id) %>% 
-  mutate(gr9.status.fall = ifelse(grade == 9, status.fall, NA),
-         gr9.status.fall = ifelse(grade == 9, status.spr, NA),
-         gr10.status.fall = ifelse(grade == 10, status.fall, NA),
-         gr10.status.fall = ifelse(grade == 10, status.spr, NA),
-         gr11.status.fall = ifelse(grade == 11, status.fall, NA),
-         gr11.status.fall = ifelse(grade == 11, status.spr, NA),
-         gr12.status.fall = ifelse(grade == 12, status.fall, NA),
-         gr12.status.fall = ifelse(grade == 12, status.spr, NA),
+  mutate(gr9.stat.fall = ifelse(grade == 9, status.fall, NA),
+         gr9.stat.spr = ifelse(grade == 9, status.spr, NA),
+         gr10.stat.fall = ifelse(grade == 10, status.fall, NA),
+         gr10.stat.spr = ifelse(grade == 10, status.spr, NA),
+         gr11.stat.fall = ifelse(grade == 11, status.fall, NA),
+         gr11.stat.spr = ifelse(grade == 11, status.spr, NA),
+         gr12.stat.fall = ifelse(grade == 12, status.fall, NA),
+         gr12.stat.spr = ifelse(grade == 12, status.spr, NA),
          any.pov = as.numeric(pov > 0),
          any.hmls = as.numeric(hmls > 0),
          any.shlt = as.numeric(shlt > 0),
@@ -70,19 +96,30 @@ doe.full <- doe.full %>%
          any.ell = as.numeric(ell > 0),
          any.shlt = ifelse(is.na(any.shlt) == T, 0, any.shlt)
          )
-# sum up absences and suspensions
+
+# sum number of days of absences and suspensions, and total suspensions
+# calculate mean percentage of days absent and suspended
 doe.full <- doe.full %>% 
   group_by(id) %>% 
-  dplyr::summarise(tot.abs = sum(abs,na.rm=T),
-                   tot.sus = sum(sus,na.rm=T),
-                   tot.sus.days = sum(sus.days,na.rm=T)) %>% 
+  dplyr::summarise(tot.abs = sum(abs, na.rm=T),
+                   av.abs = mean(per.abs, na.rm=T),
+                   tot.sus = sum(sus, na.rm=T),
+                   av.sus = mean(per.sus, na.rm=T),
+                   tot.sus.days = sum(sus.days, na.rm=T)) %>% 
   ungroup() %>% 
   right_join(doe.full)
+
+# create column for freshman year, this needs editing due to the repeated rows for people's 9th grade year
+doe.full <- doe.full %>%
+  mutate(frsh = case_when(grade == 9 ~ year - 1))
+
+# create column of age difference between NYC mandated school-age entry and grade 9-age entry
+doe.full <- doe.full %>%
+  mutate(age.diff = frsh - birth.yr - 14)
 
 # clean up columns
 doe.full <- doe.full %>% 
   select(-pov, -hmls, -shlt, -iep, -ell, -status.fall, -status.spr, -abs, -sus, -sus.days)
-
 
 # indicate flag if student ever repeated a grade
 doe.full <- doe.full %>% 
@@ -114,28 +151,8 @@ doe.full <- doe.full %>%
                    gr8.mth = tail(mth)) %>% 
   ungroup() %>% 
   right_join(doe.full)
+
 # remove grade 8 rows
 doe.full <- doe.full %>% 
   filter(grade > 8)
 
-
-# create column to flag students who repeated grades or may be entered multiple times
-
-# add a count column for how many rows each student has
-doe.full <- doe.full %>% 
-  add_count(id) %>%
-  group_by(id) %>%
-  arrange(id) %>%
-  mutate(
-    # identify students that have no school ids listed
-    noschool = case_when(is.na(sch.fall) & is.na(sch.spr) ~ 1),
-    # parse birth year to new column
-    birth.yr = year(mdy(dob)),
-    # create column for freshman year, this needs editing due to the repeated rows for people's 9th grade year
-    frsh = case_when(grade == 9 ~ year - 1),
-    # create column of age difference between NYC mandated school-age entry and grade 9-age entry
-    age.diff = frsh - birth.yr - 14,
-    # create column to show if they moved mid-year
-    mvd = case_when(sch.fall != sch.spr ~ 1,
-                    sch.fall == sch.spr ~ 0),
-  )
