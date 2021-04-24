9c3b6af7935b4ebcc0ad64e997250bb10814453f
Hope Muller <Home@GodsMerciesAreNewEveryMorning.local>
2021-04-23 21:13:32 -0400

    deleted ela and math scores, added last school attended column

--- doe.preprocess.2.R
+++ doe.preprocess.2.R
@@ -1,8 +1,10 @@
 # cleaning questions to find answers to:
+# are we eliminating kids that didn't attend 9th, 10th, etc. at NYC DOE?
+
 # why do 69 kids have more suspended days than school days? Is it cumaltive over school career?
-# some kids are listed as starting high school at age 10? they attend 75K721, a school 
+
+# kids are listed as starting high school at age 12? they attend 75K721, a school 
 # for children with severe learning disabilities.
-# should we remove schools that start with 75??
 
 # load libraries
 library(plyr)
@@ -31,7 +33,7 @@ doe.full <- raw.student
 
 # clean the data
 doe.full <- doe.full %>%
-
+  
   # rename columns
   dplyr::rename(id = RANYCSID,
                 pov = ANYPOV,
@@ -52,31 +54,31 @@ doe.full <- doe.full %>%
                 status.spr = AGDDCATJUN,
                 abs = ABSTOT,
                 sus = SUSTOT,
-                sus.days = SUSTOTDAYS,
-                ela = ELASSC,
-                mth = MTHSSC) %>%
- 
+                sus.days = SUSTOTDAYS)
+  
+  # delete ela and math scores, too many are missing
+doe.full <- doe.full %>%
+  select(-ELASSC, - MTHSSC) %>%
+  
   # subset data to grade levels used
-  filter(grade == "08" | grade == "09" | grade == "10" | grade == "11" | grade == "12")
+  filter(grade == "09" | grade == "10" | grade == "11" | grade == "12") %>%
 
   # change grade and scores to numeric values
   mutate(grade = as.numeric(grade),
-         ela = as.numeric(ela),
-         mth = as.numeric(mth),
-         # recode gender male as 0
-         gen = ifelse(gen == 2, 0, 1),
-         # code percentage days absent per year and percent days suspended per year
-         per.abs = abs/182,
-         per.sus = sus.days/182) %>%
-
+       # recode gender male as 0
+       gen = ifelse(gen == 2, 0, 1),
+       # code percentage days absent per year and percent days suspended per year
+       per.abs = abs/182,
+       per.sus = sus.days/182) %>%
+  
   # filter out students who were not going to graduate by 2019
   filter(!(grade < 12 & year == 2019) |
            (grade < 11 & year == 2018) |
            (grade < 10 & year == 2017)) %>%
-          
+  
   # parse birth year to new column
   mutate(birth.yr = year(mdy(dob))) %>%
-
+  
   # create column to show if they moved mid-year
   mutate(mvd.mid = case_when(sch.fall != sch.spr ~ 1,
                              sch.fall == sch.spr ~ 0))
@@ -105,7 +107,7 @@ doe.full <- doe.full %>%
          any.iep = as.numeric(iep > 0),
          any.ell = as.numeric(ell > 0),
          any.shlt = ifelse(is.na(any.shlt) == T, 0, any.shlt)
-         )
+  )
 
 # sum number of days of absences and suspensions, and total suspensions
 # calculate mean percentage of days absent and suspended
@@ -119,30 +121,33 @@ doe.full <- doe.full %>%
   ungroup() %>% 
   right_join(doe.full)
 
-#### add 8th grade ela and math scores here if we want them
-
-# remove grade 8 rows
-doe.full <- doe.full %>% 
-  filter(grade > 8)
-
-# column for grades completed with NYC DOE
+# column for grades completed within NYC DOE
 doe.full <- doe.full %>%
   group_by(id) %>%
   dplyr::summarise(comp.grades = n_distinct(grade)) %>%
   ungroup() %>% 
   right_join(doe.full)
 
-# create column for freshman year, this needs editing due to the repeated rows for people's 9th grade year
+# create column for freshman year, this needs editing
+# doe.full <- doe.full %>%
+#   group_by(id) %>%
+#   mutate(frsh = case_when(grade == 9 ~ year - 1),
+#          frsh = min(year)) %>%
+#   ungroup() %>% 
+#   right_join(doe.full)
+
+# create column of age difference between NYC mandated school-age entry and grade 9-age entry
+# doe.full <- doe.full %>%
+#   mutate(age.diff = frsh - birth.yr - 14)
+
+# report final school attended
 doe.full <- doe.full %>%
   group_by(id) %>%
-  mutate(frsh = case_when(grade == 9 ~ year - 1),
-         frsh = min(year)) %>%
-  ungroup() %>% 
+  select(sch.fall, year) %>%
+  rename(final.sch = sch.fall) %>%
+  filter(year == max(year)) %>%
+  select(-year) %>%
   right_join(doe.full)
-  
-# create column of age difference between NYC mandated school-age entry and grade 9-age entry
-doe.full <- doe.full %>%
-  mutate(age.diff = frsh - birth.yr - 14)
 
 # clean up columns, not deleting sus.days yet until we discuss question around why so many?
 doe.full <- doe.full %>% 
@@ -154,13 +159,9 @@ doe.f <- doe.full %>%
   group_by(id) %>%
   arrange(id) %>%
   mutate(any.repeats = case_when(n > 1 & (n_distinct(year) != n_distinct(grade)) ~ 1, 
-                             n == 1 | (n_distinct(year) == n_distinct(grade)) ~ 0)) %>% 
+                                 n == 1 | (n_distinct(year) == n_distinct(grade)) ~ 0)) %>% 
   select(-n)
 
-# report final school
-doe.full <- doe.full %>% 
-
-  
 # checkpoint, for troubleshooting
 backup <- doe.full
 doe.full <- backup
@@ -170,13 +171,6 @@ doe.full <- backup
 # THINGS THAT AREN'T WORKING PROPERLY #
 #######################################
 
-# math and ela scores then remove 8th grade 
-doe.f <- doe.full %>% 
-  group_by(id) %>% 
-  mutate(gr8.ela = case_when(grade == 8 ~ ela)) %>%
-  mutate(grade8.ela = case_when(grade != 8 ~ gr8.ela))
-ungroup() %>% 
-  right_join(doe.full)
 
 
 
