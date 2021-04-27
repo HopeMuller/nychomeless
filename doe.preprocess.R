a310d35cec67ef5eb8e90fddb07fab4b72c439a9
Hope Muller <Home@GodsMerciesAreNewEveryMorning.local>
2021-04-27 14:58:06 -0400

    edited a bunch of cleaning code, added a few variables, filtered out people who moved

--- doe.preprocess.R
+++ doe.preprocess.R
@@ -6,13 +6,17 @@ library(lubridate)
 
 # setwd to doe folder
 # Kenny: setwd("~/RANYCS/sasdata/development/kmt")
-# Hope: 
-setwd("/Users/Home/mnt/sasdata/development/kmt")
+# Hope: setwd("/Users/Home/mnt/sasdata/development/kmt")
 
 # load in data
 # nsc college attendance data
 raw.nsc <- read_csv('nsc_all.csv')
 nsc <- raw.nsc
+
+# rename is and year variables
+nsc <- nsc %>% 
+  dplyr::rename(id = RANYCSID,
+                year = YEAR)
 
 # read in school-level data
 # Hope: sch.doe <- read.csv("/Users/Home/Documents/MessyData/finalproj/DOE_schooldata.csv")
@@ -20,7 +24,7 @@ nsc <- raw.nsc
 
 # rename school column for merging
 sch.doe <- sch.doe %>%
-  rename(final.sch = DBN)
+  rename(mod.sch = DBN)
 
 # doe data
 raw.student <- foreach(year=2013:2019, .combine='rbind.fill') %do% {
@@ -31,7 +35,6 @@ raw.student <- foreach(year=2013:2019, .combine='rbind.fill') %do% {
 
 # assign student-level data to new name
 doe.full <- raw.student
-
 
 # clean the student-level data
 doe.full <- doe.full %>%
@@ -55,8 +58,10 @@ doe.full <- doe.full %>%
                 status.spr = AGDDCATJUN,
                 abs = ABSTOT,
                 sus = SUSTOT,
-                sus.days = SUSTOTDAYS) %>% 
+                sus.days = SUSTOTDAYS)
+
   # delete ela and math scores, too many are missing
+doe.full <- doe.full %>%  
   select(-ELASSC, - MTHSSC) %>% 
   # subset data to grade levels used
   filter(grade == "09" | grade == "10" | grade == "11" | grade == "12") %>%
@@ -69,21 +74,23 @@ doe.full <- doe.full %>%
          # combine absent days and suspended days as days missing from school
          missed = abs + sus.days,
          # code percentage days absent per year and percent days suspended per year
-         per.missed = missed/182,
+         per.missed = round(missed/182,2),
          per.missed = ifelse(per.missed > 1, 1, per.missed),
          # parse birth year to new column
          birth.yr = year(mdy(dob)),
          # create column to show if they moved mid-year
          mvd.mid = case_when(sch.fall != sch.spr ~ 1,
                              sch.fall == sch.spr ~ 0)) %>%
-   # filter out students listed with birthdays prior to 1994
-   filter(birth.yr > 1993)
 
 # filter out students who were not going to graduate by 2019
-doe.full <- doe.full %>% 
   filter(!(grade < 12 & year == 2019) |
          (grade < 11 & year == 2018) |
          (grade < 10 & year == 2017))
+
+# filter out students who didn't attend 9th grade in a DOE
+doe.full <- doe.full %>%
+  group_by(id) %>%
+  filter(min(grade) == 9)
 
 # column for total grades completed within NYC DOE (grades 9 - 12): visualization variable
 doe.full <- doe.full %>%
@@ -95,8 +102,8 @@ doe.full <- doe.full %>%
 # report final school attended (grade 9 - 12): visualization variable
 doe.full <- doe.full %>%
   group_by(id) %>%
-  select(sch.fall, year) %>%
-  rename(final.sch = sch.fall) %>%
+  select(sch.spr, year) %>%
+  rename(final.sch = sch.spr) %>%
   filter(year == max(year)) %>%
   select(-year) %>%
   right_join(doe.full)
@@ -107,6 +114,10 @@ doe.full <- doe.full %>%
   dplyr::summarise(final.status = last(status.spr)) %>% 
   ungroup() %>% 
   right_join(doe.full)
+
+# filter out students whose final status is moving since we don't have the outcome we need
+doe.full <- doe.full %>% 
+  filter(final.status != 4)
 
 # take out grades 11 and 12 for predictive variables below -----------------------------------
 doe.full <- doe.full %>%
@@ -134,9 +145,9 @@ doe.full <- doe.full %>%
 # calculate mean number of suspensions per year
 doe.full <- doe.full %>% 
   group_by(id) %>% 
-  dplyr::summarise(mn.missed = mean(missed, na.rm=T),
-                   av.missed = mean(per.missed, na.rm=T),
-                   mn.sus = mean(sus, na.rm=T)) %>% 
+  dplyr::summarise(mn.days.miss = round(mean(missed, na.rm=T)),
+                   av.per.miss = round(mean(per.missed, na.rm=T),2),
+                   mn.num.sus = round(mean(sus, na.rm=T))) %>% 
   ungroup() %>% 
   right_join(doe.full)
 
@@ -150,7 +161,10 @@ doe.full <- doe.full %>%
 
 # create column of age difference between NYC mandated school-age entry and grade 9-age entry
 doe.full <- doe.full %>%
-  mutate(age.diff = frsh - birth.yr - 14)
+  mutate(age.diff = frsh - birth.yr - 14) %>%
+
+# filter out students listed as starting their freshman year at age 19 or after
+  filter(age.diff < 7)
 
 # indicate flag if student repeated a grade, in grade 9 or 10
 doe.full <- doe.full %>% 
@@ -161,24 +175,29 @@ doe.full <- doe.full %>%
                                  n == 1 | (n_distinct(year) == n_distinct(grade)) ~ 0)) %>% 
   select(-n)
 
+# report final school attended (grade 9 and 10)
+doe.full <- doe.full %>%
+  group_by(id) %>%
+  select(sch.spr, year) %>%
+  rename(mod.sch = sch.spr) %>%
+  filter(year == max(year)) %>%
+  select(-year) %>%
+  right_join(doe.full)
+
 # change all NaN values in av.missed to 0
-doe.full <- doe.full %>% 
-  mutate(av.missed = ifelse(is.na(av.missed) == T, 0, av.missed))
+# doe.full <- doe.full %>% 
+#   mutate(av.missed = ifelse(is.na(av.missed) == T, 0, av.missed))
 
 # clean up columns
 doe.full <- doe.full %>% 
-  select(-pov, -hmls, -shlt, -iep, -ell, -abs, -sus, -sus.days, -missed, -tot.missed, 
+  select(-pov, -hmls, -shlt, -iep, -ell, -abs, -sus, -sus.days, -missed,
          -per.missed, -dob,-sch.fall, -sch.spr, -status.fall, -status.spr, -frsh, 
          -birth.yr, -mvd.mid)
 
 # collapsing rows, so that there is only one row per student
 doe.full <- doe.full %>% 
   group_by(id) %>% 
-  filter(row_number()==1)
-
-nsc <- nsc %>% 
-  dplyr::rename(id = RANYCSID,
-                year = YEAR)
+  filter(year == max(year))
 
 # checkpoint, for troubleshooting
 backup <- doe.full
